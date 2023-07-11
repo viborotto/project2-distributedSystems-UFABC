1401aa01c44f99c2f524f35aa41ed812b9c3daf7
viborotto <vittoriaborotto@gmail.com>
2023-07-11 18:02:35 -0300

    Ajustando PUT

--- cliente.py
+++ cliente.py
@@ -14,37 +14,59 @@ class Mensagem:
         self.message_timestamp = message_timestamp
 
 def defineServidores():
-    for i in range(3):
-        server_ip = input("Defina o IP do Servidor {i+1}:(default 127.0.0.1) ")
-        server_port = input("Defina a porta do Servidor {i+1}:(10097, 10098, 10099) ")
-        server_ips.append(server_ip)
-        server_ports.append(int(server_port))
+    # for i in range(3):
+        # server_ip = input("Defina o IP do Servidor {i+1}:(default 127.0.0.1) ")
+        # server_port = input("Defina a porta do Servidor {i+1}:(10097, 10098, 10099) ")
+        server1_ip = '127.0.0.1'
+        server1_port = 10097
+        server2_ip = '127.0.0.1'
+        server2_port = 10098
+        server3_ip = '127.0.0.1'
+        server3_port = 10099
+        server_ips.append(server1_ip)
+        server_ports.append(int(server1_port))
+        server_ips.append(server2_ip)
+        server_ports.append(int(server2_port))
+        server_ips.append(server3_ip)
+        server_ports.append(int(server3_port))
+        # server_ips.append(server_ip)
+        # server_ports.append(int(server_port))
 
 # estabelecer a conexao com um servidor, enviar a requisicao e receber a resposta
 def requisitarServidor(server_ip, server_port, mensagem):
     # TCP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        client_socket.connect((server_ip, server_port))
+        client_socket.connect((str(server_ip), int(server_port)))
+        print("mensagem: ", mensagem)
         serialized_request = pickle.dumps(mensagem)
         client_socket.sendall(serialized_request)
-        resposta_servidor_serializada = client_socket.recv(1024).decode()
-        resposta = pickle.loads(resposta_servidor_serializada)
-        print("Response:", resposta)
+        resposta_servidor_serializada = client_socket.recv(1024)
+        if len(resposta_servidor_serializada) > 0:
+            resposta = pickle.loads(resposta_servidor_serializada)
+            print("Response:", resposta)
+        else:
+            print("Empty response received from server")
+    except Exception as e:
+        print("Error communicating with server:", str(e))
+
+
     finally:
         client_socket.close()
 
 
 
 # eleger o lider aleatorio? usar uma flag de lider?
-lider_ip = server_ips[0]
-lider_port = server_ports[0]
+lider_ip = ''
+lider_port = ''
 # Executar requisições GET e PUT
 while True:
     op_kv = input("Defina a operacao e o par key-value(e.g INIT ou GET key ou PUT key=value): ")
     # op_kv = input("Defina a operacao INIT, PUT, ou GET")
-    if op_kv.lower() == 'INIT':
+    if op_kv == 'INIT':
         defineServidores()
+        lider_ip = server_ips[0]
+        lider_port = server_ports[0]
     if op_kv.lower() == 'exit':
         break
     elif op_kv.startswith('GET'):
@@ -61,9 +83,14 @@ while True:
         timestamp += 1
         mensagem = Mensagem("PUT", key_value.split('=')[0], key_value.split('=')[1], timestamp)
         # escolhe aleatoriamente um dos 3 servidores
-        id_servidor_escolhido = random.randint(0, 2)
+        # id_servidor_escolhido = random.randint(0, 2)
+        id_servidor_escolhido = 1
+        print("porta servidor escolhido: ", server_ports[id_servidor_escolhido])
+        print(type(server_ports[id_servidor_escolhido]))
+        print("ip servidor escolhido: ", server_ips[id_servidor_escolhido])
+        print(type(server_ips[id_servidor_escolhido]))
         # TODO: Na requisição do PUT, envie a key e a value.
-        requisitarServidor(id_servidor_escolhido, id_servidor_escolhido, mensagem)
+        requisitarServidor(server_ips[id_servidor_escolhido], server_ports[id_servidor_escolhido], mensagem)
 
 
 
--- servidor.py
+++ servidor.py
@@ -1,12 +1,23 @@
-import socket, os, threading, random, pickle, threading
+import socket, os, random, pickle, threading
 
 # Capturar IP e porta do servidor
 server_ip = input("Defina o IP do Servidor (default: 127.0.0.1): ") or "127.0.0.1"
 server_port = int(input("Defina a PORTA do Servidor(10097, 10098 e 10099): ") or "10097")
+print('server_ip: ', server_ip)
+print('server_port: ', server_port)
 
 # Capturar IP e porta do líder
 lider_ip = input("Defina o IP do Servidor LIDER (default: 127.0.0.1): ") or "127.0.0.1"
-lider_port = int(input("Defina a PORTA do Servidor LIDER (10097, 10098 e 10099): ") or "10097")
+lider_port = int(input("Defina a PORTA do Servidor LIDER (10097, 10098 e 10099): ") or "10098")
+print('leader_ip: ', lider_ip)
+print('lider_port: ', lider_port)
+
+class Mensagem:
+    def __init__(self, operacao, message_key, message_value, message_timestamp):
+        self.operacao = operacao
+        self.message_key = message_key
+        self.message_value = message_value
+        self.message_timestamp = message_timestamp
 
 # tabela de hash local para armazenar os pares chave-valor e os timestamps associados.
 class HashTableKV:
@@ -32,38 +43,47 @@ class HashTableKV:
             self.timestamps[key] = new_timestamp
         else:
             raise KeyError("Key does not exist in KeyValueStore")
-def processarMensagem(client_socket, address, data, key_value_store, leader_ip, leader_port):
+def processarMensagem(client_socket, address, mensagem, key_value_store, leader_ip, leader_port):
     # Implemente aqui a lógica para processar a requisição recebida do cliente
     # Se o servidor não for o líder, encaminhe a requisição para o líder
     # Caso contrário, se o comando for PUT, insira a chave-valor na tabela de hash local com o timestamp atual
     # Se o comando for GET, obtenha o valor e o timestamp associados à chave
 
     # Caso seja Put e NAO SEJA LIDER
-    if data.startswith('PUT') and (leader_ip != address[0] or leader_port != address[1]):
+    if mensagem.operacao == 'PUT' and (server_ip != lider_ip or lider_port != server_port):
         # REDIRECIONA A REQUISICAO PARA O LIDER
         print("NAO SOU O SERVIDOR LIDER")
         print("REDIRECIONANDO REQUEST PARA O LIDER")
-        response = forward_request_to_leader(leader_ip, leader_port, data)
+        response = forward_request_to_leader(leader_ip, leader_port, pickle.dumps(mensagem))
     else:
         print("SOU O SERVIDOR LIDER")
-        response = "OK"  # Exemplo de resposta para o cliente
-        if data.startswith('PUT'):
-            key_value = data[4:]  # Extrair a parte do comando após "PUT "
-            key, value = key_value.split('=')
+        if mensagem.operacao == 'PUT':
+            key = mensagem.message_key
+            value = mensagem.message_value
             timestamp = key_value_store.timestamps.get(key, 0) + 1
             # O VERIFICAR SE A KEY JA EXISTE NO HASHTABLE
-            if key_value_store.search():
+            if key_value_store.search(key):
                 # SE EXISTIR: ATUALIZA O VALOR E TIMESTAMP
                 print("chave ja existe")
-                key_value_store.update(key, value, timestamp)
-                print("atualizado o valor e timestamp da chave: " + key)
+                key_value_store.update(key, value, timestamp+1)
+                print("atualizado o valor e timestamp da chave: " + value)
+                print(key_value_store.get(key))
+                # TODO: REPLICAR VALORES E TIMESTAMP ATUALIZADO NOS OUTROS SERVIDORES
+                replicate_to_servers(key, value, timestamp, leader_ip, leader_port)
+                response = f"PUT_OK {timestamp}"
             else:
                 # INSERE KEY E VALUE EM UMA TABELA DE HASHLOCAL
                 key_value_store.put(key, value, timestamp)
-            # TODO: 2 - REPLICAR OS DADOS NOS OUTROS SERVIDORES(K, V, Timestamp)
+                print("Chave foi colocada no Hash? ", key_value_store.search(key))
+                print(key_value_store.get(key))
+                # TODO: 2 - REPLICAR OS DADOS NOS OUTROS SERVIDORES(K, V, Timestamp)
+                replicate_to_servers(key, value, timestamp, leader_ip, leader_port)
                 # Mensagem: REPLICATION key value timestamp
-        elif data.startswith('GET'):
-            key = data[4:]  # Extrair a parte do comando após "GET "
+                # Somente depois do replication enviar PUT_OK para o cliente
+                response = f"PUT_OK {timestamp}"
+                print(response)
+        elif mensagem.operacao == 'GET':
+            key = mensagem.message_key
             value, timestamp = key_value_store.get(key)
             response = f"Value: {value}, Timestamp: {timestamp}"
 
@@ -79,28 +99,34 @@ def forward_request_to_leader(leader_ip, leader_port, data):
     finally:
         leader_socket.close()
 
-def replicate_to_server(server_ip, server_port, replication_data):
+def replicate_to_server(server_ip_rep, server_port_rep, replication_data):
     replication_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        replication_socket.connect((server_ip, server_port))
+        replication_socket.connect((server_ip_rep, server_port_rep))
+        # VERIFICAR O SEND E O RECV
         replication_socket.sendall(replication_data.encode())
         response = replication_socket.recv(1024).decode()
-        print(f"Replication response from {server_ip}:{server_port}: {response}")
+        print(f"Replication response from {server_ip_rep}:{server_port_rep}: {response}")
     finally:
         replication_socket.close()
 
 def replicate_to_servers(key, value, timestamp, leader_ip, leader_port):
     servers = [
-        ("127.0.0.1", 10098),  # Endereço do servidor 2
+        ("127.0.0.1", 10097),  # Endereço do servidor 1
         ("127.0.0.1", 10099)   # Endereço do servidor 3
     ]
 
     replication_data = f"REPLICATION {key}={value} {timestamp}"
 
+    print(replication_data)
+    print(type(replication_data))
+
     replication_threads = []
-    for server_ip, server_port in servers:
-        if server_ip != leader_ip or server_port != leader_port:
-            replication_thread = threading.Thread(target=replicate_to_server, args=(server_ip, server_port, replication_data))
+    for server_ip_rep, server_port_rep in servers:
+        if server_ip_rep != leader_ip or server_port_rep != leader_port:
+            print("VERIFICA SE O IP DO SERVIDOR A SER REPLICADO É DIFERENTE DO LIDER")
+            replication_thread = threading.Thread(target=replicate_to_server,
+                                                  args=(server_ip_rep, server_port_rep, replication_data))
             replication_thread.start()
             replication_threads.append(replication_thread)
 
@@ -110,13 +136,14 @@ def replicate_to_servers(key, value, timestamp, leader_ip, leader_port):
 
 def handle_client(client_socket, address, key_value_store, leader_ip, leader_port):
     while True:
-        data = client_socket.recv(1024).decode()
-        print(f"Received data: {data} from {address[0]}:{address[1]}")
+        data = client_socket.recv(1024)
 
         if not data:
             break
 
-        processarMensagem(client_socket, address, data, key_value_store, leader_ip, leader_port)
+        mensagem = pickle.loads(data)
+        print(f"Received data: {mensagem.message_key} = {mensagem.message_value} from {address[0]}:{address[1]}")
+        processarMensagem(client_socket, address, mensagem, key_value_store, leader_ip, leader_port)
     print(f"Connection closed by {address[0]}:{address[1]}")
     client_socket.close()
 
@@ -133,7 +160,8 @@ def iniciarServidor(ip, port, leader_ip, leader_port):
         print(f"Accepted connection from {address[0]}:{address[1]}")
 
         # Iniciar uma nova thread para tratar a conexão do cliente
-        client_thread = threading.Thread(handle_client(client_socket, address, key_value_store, leader_ip, leader_port))
+        client_thread = threading.Thread(target=handle_client,
+                                         args=(client_socket, address, key_value_store, lider_ip, lider_port))
         client_thread.start()
 
 
